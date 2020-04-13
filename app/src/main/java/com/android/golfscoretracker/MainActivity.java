da36beb005e53577633ecec189483060bad98839
steven8283 <rpgtyuwert@gmail.com>
2020-04-13 20:22:22 +0800

    Second commit on April 13th 2020

--- app/src/main/java/com/android/golfscoretracker/MainActivity.java
+++ app/src/main/java/com/android/golfscoretracker/MainActivity.java
@@ -15,30 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner parSelectorA = findViewById(R.id.parSelectorForPlayerA);
-        Spinner parSelectorB = findViewById(R.id.parSelectorForPlayerB);
-
-        final String[] par = {"3 Par", "4 Par", "5 Par"};
-
-        ArrayAdapter<String> parList = new ArrayAdapter<>(MainActivity.this,
-                android.R.layout.simple_spinner_dropdown_item,
-                par);
-
-        parSelectorA.setAdapter(parList);
-        parSelectorB.setAdapter(parList);
-
-        parSelectorA.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-        });
-
-
-
-
-
-
     }
 
 
